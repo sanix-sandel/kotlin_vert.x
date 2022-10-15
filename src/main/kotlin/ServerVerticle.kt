15c568a9ec19718cdbf32de699e42e54e7a9aa0d
Sanix <sanick.ikani@rakuten.com>
2022-10-15 11:43:47 +0900

    verticles

--- src/main/kotlin/ServerVerticle.kt
+++ src/main/kotlin/ServerVerticle.kt
@@ -3,17 +3,24 @@ import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 
-val json= json{
-    obj(
-        "status" to "OK"
-    )
+
+val json=json{
+    obj("status" to "OK")
 }
-
-
 class ServerVerticle: CoroutineVerticle(){
 
     override suspend fun start(){
         val router =router()
+
+        router.mountSubRouter("/cats", catsRouter())
+
+        router.get("/status").handler{
+            ctx->ctx.response()
+            .setStatusCode(200)
+            .end(json.toString())
+        }
+
+
         vertx.createHttpServer()
             .requestHandler(router)
             .listen(8081)
@@ -24,11 +31,24 @@ class ServerVerticle: CoroutineVerticle(){
     private fun router(): Router {
         val router= Router.router(vertx)
 
-        router.get("/status").handler{
-                ctx->ctx.response()
-            .setStatusCode(200)
-            .end(json.toString())
+        return router
+    }
+
+    private fun catsRouter():Router{
+        val router=Router.router(vertx)
+
+        router.get("/").handler{
+            ctx -> ctx.response().end("Getting")
         }
+
+        router.delete("/:id").handler{
+            ctx-> ctx.response().end(json.toString())
+        }
+
+        router.put("/:id").handler{
+            ctx -> ctx.response().end(json.toString())
+        }
+
         return router
     }
 }
